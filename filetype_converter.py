2b0c647e2887c506c9d2cf97f74d40365a481fa5
Jakob Skrede <37243469+jakobskr@users.noreply.github.com>
2021-01-11 20:28:53 +0100

    Add files via upload
    Added filetype_converter.py

---
+++ filetype_converter.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def convert (old, new):
+	files = os.listdir()
+	print("."+ old)
+	for file in files:
+		if file.endswith("."+ old):
+			file_parts = file.split(".")
+			os.rename(file, file_parts[0] + "." + new)
+	
+
+if __name__ == "__main__":
+	print(sys.argv)
+	if len(sys.argv) < 3:
+		print("usage filetype_converter.py OLD NEW")
+		sys.exit()
+	convert(sys.argv[1], sys.argv[2])	
